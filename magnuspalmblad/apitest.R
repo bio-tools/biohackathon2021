1c2522e27b463d13482d4ee3abbfdb2ed145b77d
Magnus Palmblad <n.m.palmblad@lumc.nl>
2021-11-08 18:08:45 +0100

    Update apitest.R

--- magnuspalmblad/apitest.R
+++ magnuspalmblad/apitest.R
@@ -1,1 +1,16 @@
 library(jsonlite)
+library(stringr)
+
+tool <- fromJSON('https://bio.tools/api/tool/LimmaRP') # fetch bio.tools record
+
+name <- tool$credit$name[2] # extract name
+
+firstName <- unlist(str_split(name, ' '))[1] # extract first name
+
+genderize <- fromJSON(paste('https://api.genderize.io/?name=', firstName, sep=''))
+
+male <- 0
+female <- 0
+
+if(genderize$gender=='male' && genderize$probability>0.66) male=male+1
+if(genderize$gender=='female' && genderize$probability>0.66) female=female+1
