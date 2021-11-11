5eb3fa26b6de95824c17e10e036c8a875754654d
Mads Kierkegaard <mads.k@live.dk>
2021-11-11 18:32:01 +0100

    Add more imports for the package

--- Mads/biotools_statistics/__init__.py
+++ Mads/biotools_statistics/__init__.py
@@ -3,3 +3,5 @@ The package with the different functions for calculating different statistics in
 """
 
 from .time_statistics import calculate_total_entries_over_time
+from .edam_statistics import find_top_terms, get_edam_topics, get_edam_operation, get_edam_format, get_edam_data
+from .base_statistics import calculate_collection_statistics
