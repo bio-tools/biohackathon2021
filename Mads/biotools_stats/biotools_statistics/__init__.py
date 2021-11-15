ac437a60736c9c405674ada16040ae9438b9759f
Mads Kierkegaard <mads.k@live.dk>
2021-11-15 13:12:29 +0100

    Add package with a small structure
    Adds package init-file along with a utility and stats script with statistics calculation functions.

---
+++ Mads/biotools_stats/biotools_statistics/__init__.py
@@ -0,0 +1,4 @@
+"""
+The biotools_statistics package is a package for calculating different bio.tools statistics.
+"""
+
---
+++ Mads/biotools_stats/biotools_statistics/stats.py
@@ -0,0 +1,3 @@
+"""
+The script for calculating the different statistics for a given tool list.
+"""
