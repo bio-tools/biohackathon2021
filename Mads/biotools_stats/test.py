60a34f14fd115e731eb4a87ace0606d02761efaa
Mads Kierkegaard <mads.k@live.dk>
2021-11-16 21:36:52 +0100

    Add small test script

--- Mads/biotools_stats/test.py
+++ Mads/biotools_stats/test.py
@@ -1,16 +1,25 @@
 """
 The test script for the biotools_statistic package.
 Used for showing plots etc.
+
+If script is to run on other machines, please be aware of the file path.
 """
 import seaborn as sns
 import matplotlib.pyplot as plt
+import json
+
+from biotools_statistics import calculate_general_statistics
 
 
 def main():
     """
     The main entry point of the script.
-    :return:
     """
+    with open("Tools.json", "r") as f:
+        tools = json.load(f)
+
+    stats = calculate_general_statistics(tools=tools)
+    print(json.dumps(stats, indent=4))
 
 
 if __name__ == "__main__":
