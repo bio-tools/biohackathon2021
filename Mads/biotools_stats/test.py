cd17fc6bc8c08ba5c722f569ab9e4daf8cecae41
Mads Kierkegaard <mads.k@live.dk>
2021-11-16 20:55:05 +0100

    Add a main entry point method

--- Mads/biotools_stats/test.py
+++ Mads/biotools_stats/test.py
@@ -4,3 +4,14 @@ Used for showing plots etc.
 """
 import seaborn as sns
 import matplotlib.pyplot as plt
+
+
+def main():
+    """
+    The main entry point of the script.
+    :return:
+    """
+
+
+if __name__ == "__main__":
+    main()
