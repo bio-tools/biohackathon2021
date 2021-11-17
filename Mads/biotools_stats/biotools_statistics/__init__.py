b35031cb56da7958af98aaf0bab5627abaa46cdd
Mads Kierkegaard <mads.k@live.dk>
2021-11-17 20:03:40 +0100

    Import module function

--- Mads/biotools_stats/biotools_statistics/__init__.py
+++ Mads/biotools_stats/biotools_statistics/__init__.py
@@ -2,3 +2,5 @@
 The biotools_statistics package is a package for calculating different bio.tools statistics.
 """
 from .stats import calculate_general_statistics
+
+from .edam_stats import calculate_edam_term_statistics
